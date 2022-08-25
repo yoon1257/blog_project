24658d5982247acafa333e710794c04e2cf6358a
yunalee <yureve5772@gmail.com>
2022-08-25 17:19:36 +0900

    add : map()

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import { useState } from 'react';
 function App() {
   let post = "우리동네 맛집"
   let [subject, setSubject] = useState(["남자코트추천","돈까스 맛집", "경양식 맛집"]) //a는 변수라고 생각하고 b는 state 변경을 도와주는 함수 
-  let [like, setLike] = useState(0);
-  let [like2, setLike2] = useState(0);
-  let [like3, setLike3] = useState(0);//0 = 저장해놓을 값
+  let [like, setLike] = useState([0,0,0]);//0 = 저장해놓을 값
   let [modal,setModal] = useState(false)
 
  
@@ -24,7 +22,7 @@ function App() {
         setSubject(paste)
       }}>정렬</button>
 
-      <div className='list'>
+      {/* <div className='list'>
         <h4>{subject[0]}<span onClick={() => {setLike(like+1)}}>👍</span>{like}</h4>
 
         <button onClick={()=>{
@@ -32,25 +30,41 @@ function App() {
           copy[0] = "여자코트추천";
           setSubject(copy);
         }}>button</button>
-
         <p>2월 16일 발행</p>
       </div>
+
       <div className='list'>
-        <h4>{subject[1]}<span onClick={()=>{setLike2(like2+1)}}>👍</span>{like2}</h4>
+        <h4>{subject[1]}</h4>
         <p>2월 16일 발행</p>
       </div>
+      
+
       <div className='list'>
-        <h4 onClick={()=>{setModal(!modal)}}>{subject[2]}<span onClick={()=>{setLike3(like3+1)}}>👍</span>{like3}</h4>
+        <h4 onClick={()=>{setModal(!modal)}}>{subject[2]}</h4>
         <p>2월 16일 발행</p>
-      </div>
+      </div> */}
 
-    {
-      modal == true ? <Modal/> : null
-    }
+      {
+        subject.map(function(a,i){
+          return(<div className='list' key={i}>
+          <h4>{subject[i]}<span onClick={() => {
+            let copy  =[...like]
+            copy[i] = copy[i]+1 
+            setLike(copy)
+          }}>👍</span>{like[i]}</h4>
+          <p>2월 16일 발행</p>
+        </div> //a는 데이터의 값, i는 반복문을 돌때마다 증가하는 정수
+              //  그래서 a만 넣어줘도 되고 subjue변수에 i를 붙여 하나하나 증가하게 사용햐도 된다
 
+            
+          )
+        })
+      }
+   
     </div>
   );
 }
+
 
 function Modal(){
   return(
